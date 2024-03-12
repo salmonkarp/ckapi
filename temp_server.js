ae32eb256e644bde236651a7c85f23bb0907a4dd
salmonkarp <garyaxelmuliyono@gmail.com>
2024-03-12 20:45:19 +0700

    Login page ejs, css and script

--- temp_server.js
+++ temp_server.js
@@ -7,7 +7,7 @@ const app = express();
 const authenticateApiKey = require("./authenticationMiddleware");
 
 // environment variables
-const port = process.env.PORT || 3000;
+const port = 3010;
 const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
@@ -54,6 +54,7 @@ app.get("/login", (req, res) => {
 app.post("/login", (req, res) => {
   const { userType, password } = req.body;
   console.log(userType, password);
+  console.log(userType, password);
   if (userType === "order" && password === orderPassword) {
     req.session.user = userType;
     res.redirect("/orderDashboard");
@@ -64,18 +65,6 @@ app.post("/login", (req, res) => {
   }
 });
 
-// connect database first, then listen to requests
-const uri = "mongodb+srv://" + dbUser + ":" + dbPassword + "@" + dbHost;
-mongoose
-  .connect(uri)
-  .then(() => {
-    console.log("connected");
-    const server = app.listen(port, () => {
-      console.log(`Node API is running on port ${port}.`);
-    });
-    server.keepAliveTimeout = 120 * 1000;
-    server.headersTimeout = 120 * 1000;
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+const server = app.listen(port, () => {
+  console.log(`Node API is running on port ${port}.`);
+});
