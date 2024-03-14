6d549053c69d588206c748fd336e615b76269595
salmonkarp <garyaxelmuliyono@gmail.com>
2024-03-14 14:18:45 +0700

    Add Product page

--- siteRoutes/orderRoutes.js
+++ siteRoutes/orderRoutes.js
@@ -6,7 +6,7 @@ const axios = require("axios");
 const api_key = process.env.API_KEY;
 const api_url = process.env.API_URL;
 
-// Dashboard
+// Order Dashboard
 router.get("/", (req, res) => {
   const headers = {
     "x-api-key": api_key,
@@ -22,8 +22,8 @@ router.get("/", (req, res) => {
       const itemsPerPage = 5;
       const startIndex = (page - 1) * itemsPerPage;
       const endIndex = startIndex + itemsPerPage;
-      const slicedData = orderArray.slice(startIndex, endIndex)[0];
-      const totalPages = Math.ceil(orderArray.length / itemsPerPage);
+      const slicedData = orderArray[0].slice(startIndex, endIndex);
+      const totalPages = Math.ceil(orderArray[0].length / itemsPerPage);
       res.render("orderDashboard", { page, totalPages, slicedData });
     })
     .catch((error) => {
@@ -32,8 +32,37 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/addOrder", (req, res) => {
-  res.render("addOrder")
+
+// Products Dashboard
+router.get("/products", (req, res) => {
+  const headers = {
+    "x-api-key": api_key,
+  };
+  let orderArray = [];
+  axios
+    .get(api_url+'/api/product', {
+      headers,
+    })
+    .then((response) => {
+      orderArray.push(response.data);
+      const page = parseInt(req.query.page) || 1;
+      const itemsPerPage = 5;
+      const startIndex = (page - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      const slicedData = orderArray[0].slice(startIndex, endIndex);
+      const totalPages = Math.ceil(orderArray[0].length / itemsPerPage);
+      res.render("orderProduct", { page, totalPages, slicedData });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.render("error");
+    });
 })
+
+// Add Product
+router.get("/addProduct", (req,res) => {
+  res.render("productViews/addProduct");
+})
+
 
 module.exports = router;
