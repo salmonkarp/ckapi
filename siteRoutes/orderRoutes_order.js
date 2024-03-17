bdf7f8b869c92171b2be80141e9a0932e5266532
salmonkarp <garyaxelmuliyono@gmail.com>
2024-03-18 03:15:39 +0700

    Basics for Add Order Layout

--- siteRoutes/orderRoutes_order.js
+++ siteRoutes/orderRoutes_order.js
@@ -10,8 +10,40 @@ const headers = {
   "x-api-key": api_key,
 };
 
+function fetchFullData() {
+  const promises = [];
+  promises.push(axios.get(api_url + "/api/product", { headers }));
+  promises.push(axios.get(api_url + "/api/hamper", { headers }));
+  promises.push(axios.get(api_url + "/api/customer", { headers }));
+
+  return Promise.all(promises)
+    .then((responses) => {
+      const products = responses[0].data;
+      const hampers = responses[1].data;
+      const customers = responses[2].data;
+
+      return { products, hampers, customers };
+    })
+    .catch((error) => {
+      console.log(error);
+      throw error;
+    });
+}
+
 router.get("/addOrder", (req, res) => {
-  res.render("orderDashboard_addOrder");
+  fetchFullData()
+    .then((combinedData) => {
+      // console.log(combinedData);
+      res.render("orderDashboard_addOrder", {
+        products: combinedData.products,
+        hampers: combinedData.hampers,
+        customers: combinedData.customers,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.render("error", { error });
+    });
 });
 
 module.exports = router;
